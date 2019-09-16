3112f71ce8228f8485295b1f47c30a05930f8102
Justin Leonard <justinleonard@Justins-MacBook-Pro.local>
2019-09-16 08:51:52 -0400

    hi

---
+++ food.js
@@ -0,0 +1,37 @@
+//fancy array "name"+count via slack
+var inventory = [];
+var fillArray() = groceryApp {
+    inventory["water"] = 100,
+        inventory["soda"] = 100,
+        inventory["detergent"] = 50,
+        inventory["apple"] = 100,
+        inventory["banana"] = 100,
+        inventory["tomato"] = 100,
+        inventory["bread"] = 50,
+        inventory["butter"] = 50,
+        inventory["milk"] = 50,
+        inventory["chicken"] = 50;
+ }
+ alert(inventory);
+ //Add inventory count
+console.log(inventory);
+
+    function ListProducts(ProductsArray)
+    {
+    for (var i = 0; i < ProductsArray.length; i++) 
+    {
+        console.log (ProductsArray[i]);
+      }
+ }
+ inventory.push("Kiwi");
+ console.log (inventory);
+
+ function splitAndPrint(name){
+    var splitArr = name.split("");
+    var i;
+    splitArr.forEach(function(item, index, array) {
+    console.log(item);
+  });
+//fillArray(groceryApp);
+//fillArray(inventory);
+
